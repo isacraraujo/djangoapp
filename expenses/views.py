25c00e470ecd0cb7f34cecfca3d68e18e6e32aa4
isacraraujo <1s4c.3d@gmail.com>
2023-08-20 11:54:51 -0300

    Django# Expenses#Done, Currencies, Start Income

--- expenses/views.py
+++ expenses/views.py
@@ -1,30 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-#from .models import Category, Expense
+from .models import Category, Expense
+from django.contrib import messages
+from django.core.paginator import Paginator
+from userpreferences.models import UserPreference
+from django.http import JsonResponse
+import json
+from django.contrib.auth.models import User
+import datetime
+
 # Create your views here.
-#from django.contrib import messages
-#from django.contrib.auth.models import User
-#from django.core.paginator import Paginator
-#import json
-#from django.http import JsonResponse
-#from userpreferences.models import UserPreference
-#import datetime
-
-
-@login_required(login_url='/authentication/login')
-def index(request):
-    return render(request, 'expenses/index.html')
-
-def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
-
-
-
-'''
 def search_expenses(request):
     if request.method == 'POST':
         search_str = json.loads(request.body).get('searchText')
-        expenses = Expense.objects.filter(
+        expenses   = Expense.objects.filter(
             amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
             date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
             description__icontains=search_str, owner=request.user) | Expense.objects.filter(
@@ -32,16 +21,15 @@ def search_expenses(request):
         data = expenses.values()
         return JsonResponse(list(data), safe=False)
 
-
 @login_required(login_url='/authentication/login')
 def index(request):
-    categories = Category.objects.all()
-    expenses = Expense.objects.filter(owner=request.user)
-    paginator = Paginator(expenses, 5)
+    categories  = Category.objects.all()
+    expenses    = Expense.objects.filter(owner=request.user)
+    paginator   = Paginator(expenses, 5)
     page_number = request.GET.get('page')
-    page_obj = Paginator.get_page(paginator, page_number)
-    currency = UserPreference.objects.get(user=request.user).currency
-    context = {
+    page_obj    = Paginator.get_page(paginator, page_number)
+    currency    = UserPreference.objects.get(user=request.user).currency
+    context     = {
         'expenses': expenses,
         'page_obj': page_obj,
         'currency': currency
@@ -52,7 +40,7 @@ def index(request):
 @login_required(login_url='/authentication/login')
 def add_expense(request):
     categories = Category.objects.all()
-    context = {
+    context    = {
         'categories': categories,
         'values': request.POST
     }
@@ -66,8 +54,8 @@ def add_expense(request):
             messages.error(request, 'Amount is required')
             return render(request, 'expenses/add_expense.html', context)
         description = request.POST['description']
-        date = request.POST['expense_date']
-        category = request.POST['category']
+        date        = request.POST['expense_date']
+        category    = request.POST['category']
 
         if not description:
             messages.error(request, 'description is required')
@@ -78,6 +66,7 @@ def add_expense(request):
         messages.success(request, 'Expense saved successfully')
 
         return redirect('expenses')
+
 
 
 @login_required(login_url='/authentication/login')
@@ -105,23 +94,27 @@ def expense_edit(request, id):
             messages.error(request, 'description is required')
             return render(request, 'expenses/edit-expense.html', context)
 
-        expense.owner = request.user
-        expense.amount = amount
-        expense. date = date
-        expense.category = category
+        expense.owner       = request.user
+        expense.amount      = amount
+        expense.date        = date
+        expense.category    = category
         expense.description = description
 
         expense.save()
         messages.success(request, 'Expense updated  successfully')
 
         return redirect('expenses')
-
-
+    
 def delete_expense(request, id):
     expense = Expense.objects.get(pk=id)
     expense.delete()
     messages.success(request, 'Expense removed')
     return redirect('expenses')
+
+
+
+'''
+
 
 
 def expense_category_summary(request):
